67c419daf12b929ef94a6d2a78ec906853c68caa
Oleh Hudyma <olehh98@gmail.com>
2024-03-05 01:14:59 +0200

    "task3 done"

--- js_basic_task3/task3.js
+++ js_basic_task3/task3.js
@@ -1,10 +1,18 @@
-function calcNumbers(a,b){
-    let sum=[];
-    for(a;a<b;a++){
-        if(a%2 == 0){
-            sum += a + ",";
-        }
+function calcNumbers(a, b) {
+  let sum = [];
+  if (a < b) {
+    for (a; a < b; a++) {
+      if (a % 2 == 0) {
+        sum += a + ",";
+      }
     }
-    console.log(sum)
+  }else if (a > b) {
+    for (b; b < a; b++) {
+      if (b % 2 == 0) {
+        sum += b + ",";
+      }
+    }
+  }
+  return sum;
 }
-console.log(calcNumbers(40,50));
+console.log(calcNumbers(74, 32));
