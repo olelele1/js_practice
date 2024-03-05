eb401c3f2765ad68e6a96990034677c17d42e780
Oleh Hudyma <olehh98@gmail.com>
2024-03-05 19:24:04 +0200

    "changes"

--- js_basic_task3/task1.js
+++ js_basic_task3/task1.js
@@ -1,8 +1,6 @@
 function newString(str) {
     let strUpper = str.toUpperCase();
-  for (let i = 0; i < strUpper.length; i++) {
     strUpper = strUpper.replace(/[AEOUIY]/g, "*");
-  }
   return strUpper;
 }
 console.log(newString("Hello world"));
